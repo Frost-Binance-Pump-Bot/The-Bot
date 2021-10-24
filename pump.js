f8eb7bef205b79889828d2c811da57ffd8c02a84
IceWinterBot506 <mydadisbrave3@gmail.com>
2021-10-24 11:29:05 +0800

    Bot: Dev: Add experimental time stamping
    Signed-off-by: IceWinterBot506 <mydadisbrave3@gmail.com>

--- pump.js
+++ pump.js
@@ -13,6 +13,32 @@ const utils = require('./utils.js')
 // for timestamping debugging only
 // require('log-timestamp')
 
+var log = console.log;
+
+console.log = function () {
+    var first_parameter = arguments[0];
+    var other_parameters = Array.prototype.slice.call(arguments, 1);
+
+    function formatConsoleDate (date) {
+        var hour = date.getHours();
+        var minutes = date.getMinutes();
+        var seconds = date.getSeconds();
+        var milliseconds = date.getMilliseconds();
+
+        return '[' +
+               ((hour < 10) ? '0' + hour: hour) +
+               ':' +
+               ((minutes < 10) ? '0' + minutes: minutes) +
+               ':' +
+               ((seconds < 10) ? '0' + seconds: seconds) +
+               ':' +
+               ('00' + milliseconds).slice(-3) +
+               ']: ';
+    }
+
+    log.apply(console, [formatConsoleDate(new Date()) + first_parameter].concat(other_parameters));
+};
+
 const { API_KEY, API_SECRET, HTTP_INTERVAL } = config
 
 if (!API_KEY || !API_SECRET) {
