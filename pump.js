8bba621d649ac0094b380348012039a13566fc62
IceWinterBot506 <mydadisbrave3@gmail.com>
2021-10-27 09:38:28 +0800

    Bot 69
    Signed-off-by: IceWinterBot506 <mydadisbrave3@gmail.com>

--- pump.js
+++ pump.js
@@ -54,6 +54,34 @@ if (!API_KEY || !API_SECRET) {
   process.exit()
 }
 
+const prompt = require('prompt');
+
+const properties = [
+    {
+        name: 'username',
+        validator: /^[a-zA-Z\s\-]+$/,
+        warning: 'Username must be only letters, spaces, or dashes'
+    },
+    {
+        name: 'password',
+        hidden: true
+    }
+];
+
+prompt.start();
+
+prompt.get(properties, function (err, result) {
+    if (err) { return onErr(err); }
+    console.log('Command-line input received:');
+    console.log('  Username: ' + result.username);
+    console.log('  Password: ' + result.password);
+});
+
+function onErr(err) {
+    console.log(err);
+    return 1;
+}
+
 const {
   TRADE_IN,
   HARD_TAKE_PROFIT,
