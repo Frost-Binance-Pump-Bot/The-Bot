4b6d46d1295c8b8e0c200aab725b0b50adec613b
JohnCarLo <90490697+IceWinterBot506@users.noreply.github.com>
2021-11-15 15:22:12 +0800

    Update wine.sh

--- wine.sh
+++ wine.sh
@@ -1,7 +1,5 @@
-sudo apt-get update
-sudo apt-get install wine -y
-sudo dpkg --add-architecture i386
-sudo apt-get update
-sudo apt-get install wine32 -y
-
-# wine sniper.py /nogui
+sudo apt-get update;
+sudo apt-get install wine -y;
+sudo dpkg --add-architecture i386;
+sudo apt-get update;
+sudo apt-get install wine32 -y; clear
